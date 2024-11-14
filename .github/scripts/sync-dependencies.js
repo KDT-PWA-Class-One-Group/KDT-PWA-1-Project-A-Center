52ed09f085735f3df3efd07d74ce9d7720a1e8b2
kongukjae <kongukjae@gmail.com>
2024-11-14 18:07:46 +0900

    fix : CI script 수정

--- .github/scripts/sync-dependencies.js
+++ .github/scripts/sync-dependencies.js
@@ -6,32 +6,50 @@ const centerPackageJson = JSON.parse(
   fs.readFileSync('package.json', 'utf8')
 );
 
-// 동기화할 프로젝트 목록
-const projects = ['Project-A-Left', 'Project-A-Right'];
+// 자식 프로젝트들의 경로와 정보
+const projects = [
+  {
+    dir: 'Project-A-Left',
+    name: 'KDT-PWA-1-Project-A-Team-Left'
+  },
+  {
+    dir: 'Project-A-Right',
+    name: 'KDT-PWA-1-Project-A-Team-Right'
+  }
+];
 
-projects.forEach(projectName => {
-  const projectPackageJsonPath = path.join(projectName, 'package.json');
+projects.forEach(project => {
+  const packageJsonPath = path.join(project.dir, 'package.json');
+  console.log(`Processing ${packageJsonPath}...`);
 
-  // 자식 프로젝트의 package.json 읽기
-  const projectPackageJson = JSON.parse(
-    fs.readFileSync(projectPackageJsonPath, 'utf8')
-  );
+  try {
+    // 자식 프로젝트의 package.json 읽기
+    const projectPackageJson = JSON.parse(
+      fs.readFileSync(packageJsonPath, 'utf8')
+    );
 
-  // dependencies 동기화
-  projectPackageJson.dependencies = {
-    ...projectPackageJson.dependencies,
-    ...centerPackageJson.dependencies
-  };
+    // 기존 정보 유지하면서 dependencies와 devDependencies 업데이트
+    const updatedPackageJson = {
+      ...projectPackageJson,
+      dependencies: {
+        ...(projectPackageJson.dependencies || {}),
+        ...(centerPackageJson.dependencies || {})
+      },
+      devDependencies: {
+        ...(projectPackageJson.devDependencies || {}),
+        ...(centerPackageJson.devDependencies || {})
+      }
+    };
 
-  // devDependencies 동기화
-  projectPackageJson.devDependencies = {
-    ...projectPackageJson.devDependencies,
-    ...centerPackageJson.devDependencies
-  };
+    // 업데이트된 package.json 저장
+    fs.writeFileSync(
+      packageJsonPath,
+      JSON.stringify(updatedPackageJson, null, 2) + '\n'
+    );
 
-  // 업데이트된 package.json 저장
-  fs.writeFileSync(
-    projectPackageJsonPath,
-    JSON.stringify(projectPackageJson, null, 2) + '\n'
-  );
+    console.log(`Successfully updated ${project.name} dependencies`);
+  } catch (error) {
+    console.error(`Error processing ${project.name}:`, error);
+    process.exit(1);
+  }
 });
